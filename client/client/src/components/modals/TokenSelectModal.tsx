5db3a881b7b2ed712c41f7a723866c7ac5153ca5
Chinaskihate <islam.kadyrov.dlschool@gmail.com>
2023-03-13 18:08:13 +0300

    changed modal

--- client/client/src/components/modals/TokenSelectModal.tsx
+++ client/client/src/components/modals/TokenSelectModal.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Button, Modal} from "react-bootstrap";
+import {Button, Container, Modal, Row} from "react-bootstrap";
 import './TokenSelectModal.css';
 
 const TokenSelectModal = () => {
@@ -14,24 +14,22 @@ const TokenSelectModal = () => {
                 Select token
             </Button>
 
-            <Modal show={show} onHide={handleClose}>
+            <Modal className="my-modal" show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>Select token</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <div>Token 1</div>
-                    <div>Token 2</div>
-                    <div>Token 3</div>
-                    <div>Token 4</div>
-                    <div>Token 5</div>
+                    <Container>
+                        <Row>1</Row>
+                    </Container>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
-                    </Button>
+                    <div>
+                        <Button className="col-12 rounded-default" onClick={handleClose}>Cancel</Button>
+                    </div>
+                    <div>
+                        <Button className="col-12 rounded-default" onClick={handleClose}>Select</Button>
+                    </div>
                 </Modal.Footer>
             </Modal>
         </>

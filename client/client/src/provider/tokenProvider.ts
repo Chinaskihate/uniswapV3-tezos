740cb60bed7a9501aa54f43221400af5142320b5
Sergey <zubby749@gmail.com>
2023-03-31 18:19:53 +0300

    axios changed to simple fetch

--- client/client/src/provider/tokenProvider.ts
+++ client/client/src/provider/tokenProvider.ts
@@ -1,29 +1,33 @@
-import axios from "axios";
 import {TokenWithExtendedStatistics} from "../entities/tokenWithExtendedStatistics";
 import {TokenWithBaseStatistics} from "../entities/tokenWithBaseStatistics";
 
 export class TokenProvider {
   public async findAllByNames(): Promise<TokenWithBaseStatistics[]> {
     try {
-      const response = await axios.get<TokenWithBaseStatistics[]>(
-        'http://localhost:3001/api/Token/v1/names',
-        { headers: { 'Access-Control-Allow-Origin': true }});
-      return response.data
+      const response = await fetch('http://localhost:3001/api/Token/v1/names', {
+        method: 'GET',
+        headers: { 'Access-Control-Allow-Origin': '*' },
+      });
+      return await response.json();
     } catch (error) {
       console.error(error);
       return [] as TokenWithBaseStatistics[];
     }
   }
 
+
   public async findByAddress(address: string): Promise<TokenWithExtendedStatistics> {
     try {
-      const response = await axios.get<TokenWithExtendedStatistics>(
-        'http://localhost:3001/api/Token/v1/'+ address,
-        { headers: { 'Access-Control-Allow-Origin': true }});
-      return response.data
+      const response = await fetch('http://localhost:3001/api/Token/v1/' + address, {
+        method: 'GET',
+        headers: { 'Access-Control-Allow-Origin': '*' },
+      });
+      return await response.json();
     } catch (error) {
       console.error(error);
-      return {} as TokenWithExtendedStatistics
+      return {} as TokenWithExtendedStatistics;
     }
   }
+
+
 }
